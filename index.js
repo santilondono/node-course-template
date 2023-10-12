91ec184295d707363fe520e95a03c8d85118a74d
Santiago <santiloo2002@gmail.com>
2023-10-11 20:39:38 -0500

    borramos el codigo de index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const http = require("node:http");
-const server = http.createServer((peticion, respuesta) => {
-	const url = peticion.url;
-	if (url == "/") {
-		respuesta.end("¡Bienvenido al servidor de node!");
-	}
-});
-const port = 3000;
-server.listen(port, () => {
-	console.log("Servidor iniciado en el puerto 3000");
-});
+import Express from "express"
